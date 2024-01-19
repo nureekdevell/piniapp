e0f83e7f5b61ce281f5815de8860998eabc48bd6
nureekdevell <58046604+nureekdevell@users.noreply.github.com>
2024-01-19 15:39:24 +0500

    initial

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,38 @@
 <template>
   <div class="container">
-<h1 class="text-center">Hello Pinia</h1>
-    <h2 class="text-center">{{counterApp.counter}}</h2>
-<div class="text-center">
-  <button @click="counterApp.increment"  class="btn m-2 btn-primary">increm</button>
-  <button  @click="counterApp.decrement" class="btn btn-danger">decrem</button></div>
+<h1 class="text-center">Todo App</h1>
+<div class="row">
+  <form class="col-md-6 offset-3" @submit="counterApp.addUser">
+    <input class="form-control" placeholder="Enter Name" type="text" v-model="counterApp.name">
+    <input class="form-control my-2" placeholder="Enter @mail" type="text" v-model="counterApp.email">
+    <input class="form-control" placeholder="Enter Job" type="text" v-model="counterApp.job">
+    <button type="submit" class="btn btn-primary my-2">Add user</button>
+  </form>
+
+  <div class="col-md-6 offset-3">
+    <table class="table">
+      <thead>
+      <tr>
+        <th scope="col">#id</th>
+        <th scope="col">Name</th>
+        <th scope="col">Email</th>
+        <th scope="col">Job</th>
+      </tr>
+      </thead>
+      <tbody>
+      <tr>
+        <th scope="row">1</th>
+        <td>nme</td>
+        <td>Otto</td>
+        <td>@mdo</td>
+      </tr>
+
+      </tbody>
+    </table>
+  </div>
+</div>
+
+
 
   </div>
 </template>
--- src/stores/counter.js
+++ src/stores/counter.js
@@ -2,17 +2,39 @@ import {defineStore} from "pinia";
 export const useCounterStore = defineStore('counterStore', {
     state: () => {
     return{
-        counter: 1
+        name: '',
+        email: '',
+        job: '',
+        users:[]
     }
     },
     actions: {
-        increment() {
-            return this.counter++
-        },
-        decrement() {
-            return this.counter--
+    addUser(event) {
+    event.preventDefault()
+        let newUser = {
+            id:this.users.length+1,
+            name:this.name,
+            email:this.email,
+            job:this.job
         }
-    }
+        if (this.name.trim() === '' || this.email.trim === '' || this.job.trim()=== '') {
+            alert('iltimos Inputlarga bush joy qoldirmang')
+            return
+        }if (!this.email.includes('@gmail')) {
+            alert('Iltimos @ kiriting')
+            return
+        }
+        this.users.push(newUser)
+        this.name = ''
+        this.email = ''
+        this.job = ''
+}
+    },
+    getters: {
+
+
+        }
+
 
 
 })
